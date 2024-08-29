8981bd5c81e051976fb41f98630b45d62c687121
Juan Torres <juan93git@gmail.com>
2024-08-29 14:02:12 +0200

    Better display of date and hour when plotting progress.

--- src/components/exerciseProgressPlot/ExerciseProgressPlot.js
+++ src/components/exerciseProgressPlot/ExerciseProgressPlot.js
@@ -9,8 +9,29 @@ import {
 } from 'recharts';
 import styles from './ExerciseProgressPlot.module.css';
 
+const formatDate = (date) => {
+  const formattedDate = date.toLocaleDateString();
+  const formattedTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+  return { formattedDate, formattedTime };
+};
+
+const CustomTick = ({ x, y, payload }) => {
+  const { formattedDate, formattedTime } = formatDate(new Date(payload.value));
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={0} y={0} textAnchor="middle" fill="#666">
+        {/* Adjust dy values to control spacing between date and time */}
+        <tspan x="0" dy="1.2em">{formattedDate}</tspan>
+        <tspan x="0" dy="1.6em">{formattedTime}</tspan>
+      </text>
+    </g>
+  );
+};
+
 function ExerciseProgressPlot({ exerciseName, data }) {
-  // Get values from css variables in variables.css file
+  // Get values from CSS variables in variables.css file
   const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color');
   const backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color');
   const accentColor = getComputedStyle(document.documentElement).getPropertyValue('--accent-color');
@@ -31,30 +52,23 @@ function ExerciseProgressPlot({ exerciseName, data }) {
 
   // Calculate the maximum number of sets in the data to dynamically generate bars in the chart
   const setCount = data.reduce((acc, item) => {
-    // Extract keys from each data item
     const keys = Object.keys(item);
-    // Find the highest set number in the current item
     const maxSet = keys
       .filter(key => key.includes('_set_'))
       .reduce((max, key) => {
-        // Extract set number from the key and update max if this is the highest so far
         const setNumber = parseInt(key.split('_')[2], 10);
         return Math.max(max, setNumber);
       }, 0);
-    // Update the overall maximum set count if this item's max is higher
     return Math.max(acc, maxSet);
   }, 0);
 
   // Generate a Bar component for each set found in the data
   const bars = Array.from({ length: setCount }, (_, i) => (
-    // Key is important for React's rendering engine to manage list updates efficiently
     <Bar key={`set-${i + 1}`} dataKey={`reps_set_${i + 1}`} fill={primaryColor}>
-      {/* LabelList displays additional data (in this case, weight) on each bar */}
       <LabelList dataKey={`weight_set_${i + 1}`} content={renderCustomizedLabel} />
     </Bar>
   ));
 
-  // Add some top room for the highest values
   // Calculate the maximum value across all sets
   const maxValue = data.reduce((max, item) => {
     const itemMax = Object.keys(item)
@@ -65,12 +79,9 @@ function ExerciseProgressPlot({ exerciseName, data }) {
 
   // Add a buffer to the maximum value, e.g., 10% of the maxValue
   const buffer = maxValue * 0.1;
-
   const adjustedMax = Math.ceil(maxValue + buffer);
 
-
   return (
-    // TODO Use ResponsiveContainer to make the chart responsive. In my previous attempts, the chart just disappeared.
     <div className={styles.chartContainer}>
       <span className={styles.chartTitle}>{exerciseName}</span>
       <ComposedChart
@@ -80,30 +91,30 @@ function ExerciseProgressPlot({ exerciseName, data }) {
         margin={{
           top: 20,
           right: 20,
-          bottom: 25,
+          bottom: 50,  // Increase bottom margin for better visibility
           left: 20,
         }}
       >
         <CartesianGrid stroke="#f5f5f5" />
-        <XAxis dataKey="date" label={{ value: 'Day', position: 'insideBottom', offset: -20 }} tickMargin={10} />
+        <XAxis
+          dataKey="date"
+          label={{ value: 'Date', position: 'insideBottom', offset: -40 }} // Adjust offset for correct label placement
+          tickMargin={5} // Increase margin for better separation
+          tick={<CustomTick />}
+        />
         <YAxis label={{ value: 'Reps', angle: -90, position: 'insideLeft', dx: -10 }} dx={-15} domain={[0, adjustedMax]} />
         <Tooltip
           formatter={(value, name) => {
-            // Check if the name matches the pattern for sets (e.g., "reps_set_1")
             if (name.startsWith('reps_set_')) {
-              // Extract the set number from the name
               const setNumber = name.split('_')[2];
-              // Customize the label
               return [
                 value,
                 `Set ${setNumber} reps`,
               ];
             }
-            // For other data keys, return the value and name without changes
             return [value, name];
           }}
-
-          contentStyle={{ backgroundColor: `${backgroundColor}E8` }} // Adding transparency to the background
+          contentStyle={{ backgroundColor: `${backgroundColor}E8` }}
         />
         {bars}
       </ComposedChart>
