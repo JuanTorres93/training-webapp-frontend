ea87fde6243a81d1ccaba7d0925c1feb81bc5b0d
Juan Torres <juan93git@gmail.com>
2024-09-13 13:06:26 +0200

    Hero section

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,7 +1,14 @@
+import styles from "./HeroSection.module.css";
+
 function HeroSection() {
     return (
-        <div>
-            Hero section
+        <div className={styles.container}>
+            <div className={styles.heroText}>
+                Home workouts tracked, progress <span className={styles.heroAccent}>unleashed!</span>
+            </div>
+            <div className={styles.heroImg}>
+                <img src="/images/hero.png" alt="Hero" />
+            </div>
         </div>
     );
 };
