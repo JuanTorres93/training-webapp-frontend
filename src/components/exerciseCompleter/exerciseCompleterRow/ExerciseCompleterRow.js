1c869aacde966135b15005bec45a4defd14368bd
Juan Torres <juan93git@gmail.com>
2024-09-25 20:54:53 +0200

    Refactor ExerciseCompleterRow component to update workout progress color dynamically

--- src/components/exerciseCompleter/exerciseCompleterRow/ExerciseCompleterRow.js
+++ src/components/exerciseCompleter/exerciseCompleterRow/ExerciseCompleterRow.js
@@ -1,60 +1,55 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { updateActiveWorkoutExercise } from '../../../features/workouts/workoutSlice';
-
 import styles from './ExerciseCompleterRow.module.css';
 
 function ExerciseCompleterRow({ exerciseId, exerciseOrder, setNumber, placeholderWeight, placeholderReps }) {
     const dispatch = useDispatch();
 
+    const [weight, setWeight] = useState('');
+    const [reps, setReps] = useState('');
+
     const weightId = `weight-${exerciseId}-${exerciseOrder}-${setNumber}`;
     const repsId = `reps-${exerciseId}-${exerciseOrder}-${setNumber}`;
 
-    const handleChange = (event) => {
-        const target = event.target;
+    const handleWeightChange = (event) => {
+        const newWeight = event.target.value;
+        setWeight(newWeight);
+        updateWorkout(newWeight, reps);
+    };
 
-        // split target.id by '-' and get the last three elements as exerciseId, exerciseOrder and setNumber
-        const [_, id, order, set] = target.id.split('-');
+    const handleRepsChange = (event) => {
+        const newReps = event.target.value;
+        setReps(newReps);
+        updateWorkout(weight, newReps);
+    };
 
-        // find weight and reps by target.id
-        const weightComponent = document.getElementById(`weight-${id}-${order}-${set}`)
-        const repsComponent = document.getElementById(`reps-${id}-${order}-${set}`)
-
-        let weight = weightComponent.value;
-        let reps = repsComponent.value;
-
-        weight = weight === '' ? 0 : parseFloat(weight);
-        reps = reps === '' ? 0 : parseInt(reps);
-
-        if (weight === 0) {
-            weightComponent.style.color = '';
-        }
-        else if (weight > placeholderWeight) {
-            weightComponent.style.color = 'green';
-        } else if (weight === placeholderWeight) {
-            weightComponent.style.color = '';
-        } else {
-            weightComponent.style.color = 'red';
-        }
-
-        if (reps === 0) {
-            repsComponent.style.color = '';
-        }
-        else if (reps > placeholderReps) {
-            repsComponent.style.color = 'green';
-        } else if (reps === placeholderReps) {
-            repsComponent.style.color = '';
-        } else {
-            repsComponent.style.color = 'red';
-        }
+    const updateWorkout = (newWeight, newReps) => {
+        const newWeightProcessed = newWeight === '' ? 0 : parseFloat(newWeight);
+        const newRepsProcessed = newReps === '' ? 0 : parseInt(newReps);
 
         dispatch(updateActiveWorkoutExercise({
-            exerciseId: id,
-            exerciseOrder: order,
-            setNumber: set,
-            weight,
-            reps,
+            exerciseId,
+            exerciseOrder,
+            setNumber,
+            weight: newWeightProcessed,
+            reps: newRepsProcessed,
         }));
     };
+
+    const weightColor = weight == placeholderWeight
+        ? ''
+        : weight > placeholderWeight
+            ? styles.progressing
+            : styles.notProgressing;
+
+    const repsColor = weightColor === styles.progressing
+        ? styles.progressing
+        : reps == placeholderReps
+            ? ''
+            : reps > placeholderReps
+                ? styles.progressing
+                : styles.notProgressing;
 
     return (
         <div className={styles.row}>
@@ -70,9 +65,13 @@ function ExerciseCompleterRow({ exerciseId, exerciseOrder, setNumber, placeholde
                     id={weightId}
                     type="number"
                     placeholder={placeholderWeight}
-                    onChange={handleChange}
+                    value={weight}
+                    onChange={handleWeightChange}
                     inputMode='decimal'
                 />
+                <span className={`${styles.label} ${weightColor} ${styles.bold}`}>
+                    ({placeholderWeight} prev)
+                </span>
             </div>
 
             <div className={`${styles.fieldContainer} ${styles.userInput}`}>
@@ -82,9 +81,14 @@ function ExerciseCompleterRow({ exerciseId, exerciseOrder, setNumber, placeholde
                     id={repsId}
                     type="number"
                     placeholder={placeholderReps}
-                    onChange={handleChange}
+                    value={reps}
+                    onChange={handleRepsChange}
                     inputMode='numeric'
                 />
+                <span
+                    className={`${styles.label} ${repsColor} ${styles.bold}`}>
+                    ({placeholderReps} prev)
+                </span>
             </div>
         </div>
     );
