8a033bb742730e2a1cd1db4a54f8da7e1a33582c
Juan Torres <juan93git@gmail.com>
2024-10-02 12:14:21 +0200

    Handle failure on login with OAuth.

--- src/routes/loginPage/LoginPage.js
+++ src/routes/loginPage/LoginPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import PagePresenter from '../../components/pagePresenter/PagePresenter'
 import LoginForm from '../../components/loginForm/LoginForm'
 import OAuthLogin from '../../components/oauthLogin/OAuthLogin'
+import Alert from '../../components/modals/alert/Alert'
 
 import styles from './LoginPage.module.css'
 
@@ -12,32 +15,62 @@ import { serverBaseURL } from '../../serverAPI/serverAPIConfig'
 import { selectUser, selectUserIsLoading } from '../../features/user/userSlice'
 
 const LoginPage = () => {
+    const [isAlertOpen, setIsAlertOpen] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
     const user = useSelector(selectUser);
     const userIsLoading = useSelector(selectUserIsLoading);
 
-    return (
-        <PagePresenter children={
-            <div className={styles.pageContainer}>
-                <h2 className='heading'>Login</h2>
-                <LoginForm
-                    user={user}
-                    userIsLoading={userIsLoading}
-                />
+    useEffect(() => {
+        const queryParams = new URLSearchParams(location.search);
 
-                <div className={styles.oauthContainer}>
-                    <OAuthLogin
-                        callbackUrl={`${serverBaseURL}/login/google`}
-                        serviceName="Google"
-                        logoPath="favicon.ico"
+        if (queryParams.size > 0) {
+            try {
+                const msg = queryParams.get("msg");
+                if (msg === 'Login failed') setIsAlertOpen(true);
+            } catch (error) {
+                setIsAlertOpen(false);
+            }
+        }
+
+    }, [location]);
+
+    return (
+        <>
+            <PagePresenter children={
+                <div className={styles.pageContainer}>
+                    <h2 className='heading'>Login</h2>
+                    <LoginForm
+                        user={user}
+                        userIsLoading={userIsLoading}
                     />
-                    {/* <OAuthLogin
+
+                    <div className={styles.oauthContainer}>
+                        <OAuthLogin
+                            callbackUrl={`${serverBaseURL}/login/google`}
+                            serviceName="Google"
+                            logoPath="/favicon.ico"
+                        />
+                        {/* <OAuthLogin
                         callbackUrl={`${serverBaseURL}/login/facebook`}
                         serviceName="Facebook"
                         logoPath="logo192.png"
                     /> */}
+                    </div>
                 </div>
-            </div>
-        } />
+            } />
+
+
+            <Alert
+                isOpen={isAlertOpen}
+                onRequestClose={() => {
+                    setIsAlertOpen(false)
+                    navigate('/login')
+                }}
+                message='Login failed.'
+            />
+        </>
     )
 }
 
