83e75621abe63639c16724d2f75b4be72b4e9d10
Juan Torres <juan93git@gmail.com>
2024-11-26 13:56:02 +0100

    Refactored template creator layout.

--- src/components/templateCreator/TemplateCreator.js
+++ src/components/templateCreator/TemplateCreator.js
@@ -9,46 +9,42 @@ const TemplateCreator = ({
     return (
         // TODO HACERLO TODO UNA SOLA GRID PARA MEJOR ALINEACIÓN
         <div className="template-creator">
-            <div className="template-creator__exercises-box template-creator__subgrid">
-                <TranslatedSearchBar
-                    extraClasses="template-creator__exercise-search-bar"
-                />
-                <TranslatedButtonNew
-                    extraClasses="template-creator__exercise-button-new"
-                // onClick={handleClickShowPopup}
-                />
-                <div className="template-creator__available-exercises-box">
-                    {/* TODO use ExercisePresenter component */}
-                    AVAILABLE EXERCISE
-                </div>
+            <TranslatedSearchBar
+                extraClasses="template-creator__exercise-search-bar"
+            />
+            <TranslatedButtonNew
+                extraClasses="template-creator__exercise-button-new"
+            // onClick={handleClickShowPopup}
+            />
+            <div className="template-creator__available-exercises-box">
+                {/* TODO use ExercisePresenter component */}
+                AVAILABLE EXERCISE
             </div>
-            <div className="template-creator__templates-box template-creator__subgrid">
-                <div className="template-creator__input-box">
-                    <label htmlFor="name" className="template-creator__label">{nameLabel}</label>
-                    <input
-                        id="name"
-                        className="base-input-text template-creator__input"
-                        type="text"
-                        placeholder={nameLabel}
-                        maxLength={40}
-                    />
-                </div>
+            <div className="template-creator__input-box">
+                <label htmlFor="name" className="template-creator__label">{nameLabel}</label>
+                <input
+                    id="name"
+                    className="base-input-text template-creator__input"
+                    type="text"
+                    placeholder={nameLabel}
+                    maxLength={40}
+                />
+            </div>
 
-                <div className="template-creator__input-box">
-                    <label htmlFor="description" className="template-creator__label">{descriptionLabel}</label>
-                    <textarea
-                        id="description"
-                        className="base-input-text template-creator__input"
-                        placeholder={descriptionLabel}
-                        maxLength={500}
-                    >
+            <div className="template-creator__input-box">
+                <label htmlFor="description" className="template-creator__label">{descriptionLabel}</label>
+                <textarea
+                    id="description"
+                    className="base-input-text template-creator__input"
+                    placeholder={descriptionLabel}
+                    maxLength={500}
+                >
 
-                    </textarea>
-                </div>
-                <div className="template-creator__used-exercises-box">
-                    {/* TODO use ExercisePresenter component */}
-                    USED EXERCISE
-                </div>
+                </textarea>
+            </div>
+            <div className="template-creator__used-exercises-box">
+                {/* TODO use ExercisePresenter component */}
+                USED EXERCISE
             </div>
         </div>
     );
