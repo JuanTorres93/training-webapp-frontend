2c18c1f098c2cef4b06aaba8e6107e3809774428
Juan Torres <juan93git@gmail.com>
2024-12-02 09:28:50 +0100

    Started to implement line graph

--- src/routes/homePage/HomePageV2.js
+++ src/routes/homePage/HomePageV2.js
@@ -1,7 +1,64 @@
 import TranslatedNavVertical from "../../components/navVertical/TranslatedNavVertical";
+import LineGraph from "../../components/lineGraph/LineGraph";
 
 export default function HomePageV2() {
     // TODO only appear if user is logged in
+
+    const data = [
+        {
+            "id": "japan",
+            "data": [
+                {
+                    "x": "plane",
+                    "y": 36
+                },
+                {
+                    "x": "helicopter",
+                    "y": 25
+                },
+                {
+                    "x": "boat",
+                    "y": 191
+                },
+                {
+                    "x": "train",
+                    "y": 246
+                },
+                {
+                    "x": "subway",
+                    "y": 100
+                },
+                {
+                    "x": "bus",
+                    "y": 274
+                },
+                {
+                    "x": "car",
+                    "y": 220
+                },
+                {
+                    "x": "moto",
+                    "y": 126
+                },
+                {
+                    "x": "bicycle",
+                    "y": 29
+                },
+                {
+                    "x": "horse",
+                    "y": 294
+                },
+                {
+                    "x": "skateboard",
+                    "y": 189
+                },
+                {
+                    "x": "others",
+                    "y": 40
+                }
+            ]
+        },
+    ];
 
     return (
         <div className="behind-app">
@@ -9,7 +66,8 @@ export default function HomePageV2() {
                 <TranslatedNavVertical />
                 <section className="home-page">
                     {/* TODO DELETE */}
-                    HOME PAGE
+                    HOME PAGE HOLA
+                    <LineGraph data={data} />
                 </section>
             </main>
         </div>
