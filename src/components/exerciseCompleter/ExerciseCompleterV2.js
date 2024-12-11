368543c9afeaa025322834fe2919fb0a97947d85
Juan Torres <juan93git@gmail.com>
2024-12-11 09:05:04 +0100

    ExerciseCompleter: Chart responsive in vertical direction

--- src/components/exerciseCompleter/ExerciseCompleterV2.js
+++ src/components/exerciseCompleter/ExerciseCompleterV2.js
@@ -61,6 +61,75 @@ const ExerciseCompleterV2 = ({ previousData, ticksCountYAxis }) => {
                         placeholder="Reps"
                     />
                 </div>
+
+                <div className="exercise-completer__set-number">
+                    2
+                </div>
+                <div className="exercise-completer__weight">
+                    {/* TODO and set number to name */}
+                    <input
+                        className="base-input-text integer-input exercise-completer__input"
+                        type="number"
+                        name="weight"
+                        placeholder="Weight"
+                    />
+                </div>
+                <div className="exercise-completer__reps">
+                    {/* TODO and set number to name */}
+                    {/* TODO last session value as placeholder */}
+                    <input
+                        className="base-input-text integer-input exercise-completer__input"
+                        type="number"
+                        name="reps"
+                        placeholder="Reps"
+                    />
+                </div>
+
+                <div className="exercise-completer__set-number">
+                    2
+                </div>
+                <div className="exercise-completer__weight">
+                    {/* TODO and set number to name */}
+                    <input
+                        className="base-input-text integer-input exercise-completer__input"
+                        type="number"
+                        name="weight"
+                        placeholder="Weight"
+                    />
+                </div>
+                <div className="exercise-completer__reps">
+                    {/* TODO and set number to name */}
+                    {/* TODO last session value as placeholder */}
+                    <input
+                        className="base-input-text integer-input exercise-completer__input"
+                        type="number"
+                        name="reps"
+                        placeholder="Reps"
+                    />
+                </div>
+
+                <div className="exercise-completer__set-number">
+                    2
+                </div>
+                <div className="exercise-completer__weight">
+                    {/* TODO and set number to name */}
+                    <input
+                        className="base-input-text integer-input exercise-completer__input"
+                        type="number"
+                        name="weight"
+                        placeholder="Weight"
+                    />
+                </div>
+                <div className="exercise-completer__reps">
+                    {/* TODO and set number to name */}
+                    {/* TODO last session value as placeholder */}
+                    <input
+                        className="base-input-text integer-input exercise-completer__input"
+                        type="number"
+                        name="reps"
+                        placeholder="Reps"
+                    />
+                </div>
             </div>
 
             <div className="exercise-completer__chart-box">
