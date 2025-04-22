2f3a7aab7442da0012e2f770fbbeb33b994824c4
Juan Torres <juan93git@gmail.com>
2025-04-22 07:43:13 +0200

    Responsive login and sign up pages

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,87 +1,112 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Link as ScrollLinkToId } from "react-scroll";
-import { US, ES } from 'country-flag-icons/react/3x2'
+import { US, ES } from "country-flag-icons/react/3x2";
 
 const scrollConfig = {
-    spy: true,
-    smooth: true,
-    duration: 500,
-}
-
+  spy: true,
+  smooth: true,
+  duration: 500,
+};
 
 const NavHorizontal = ({
-    items, // array of objects with text and (id OR path). id is used for react-scroll, path is used for react-router-dom
-    currentLocation,    // current path to determine if logo should be a link or a scroll link
-    userIsLoggedIn = false,
-    loginText = "",
-    signUpText = "",
-    currentLanguage = "",
-    cbChangeLanguage = () => { } }) => {
+  items, // array of objects with text and (id OR path). id is used for react-scroll, path is used for react-router-dom
+  currentLocation, // current path to determine if logo should be a link or a scroll link
+  userIsLoggedIn = false,
+  loginText = "",
+  signUpText = "",
+  currentLanguage = "",
+  cbChangeLanguage = () => {},
+}) => {
+  return (
+    <nav className="nav-horizontal">
+      {currentLocation === "/" ? (
+        <ScrollLinkToId
+          to="hero-section"
+          className="nav-horizontal__logo-box"
+          {...scrollConfig}
+        >
+          <img
+            src="/images/logos/logo_border_small.svg"
+            alt="logo"
+            className="nav-horizontal__logo"
+          />
+        </ScrollLinkToId>
+      ) : (
+        <Link to="/" className="nav-horizontal__logo-box">
+          <img
+            src="/images/logos/logo_border_small.svg"
+            alt="logo"
+            className="nav-horizontal__logo"
+          />
+        </Link>
+      )}
 
-    return (
-        <nav className="nav-horizontal">
-            {currentLocation === "/" ?
-                <ScrollLinkToId to="hero-section" className="nav-horizontal__logo-box" {...scrollConfig}>
-                    <img src="/images/logos/logo_border_small.svg" alt="logo" className="nav-horizontal__logo" />
-                </ScrollLinkToId> :
-                <Link to="/" className="nav-horizontal__logo-box">
-                    <img src="/images/logos/logo_border_small.svg" alt="logo" className="nav-horizontal__logo" />
+      {items ? (
+        <ul className="nav-horizontal__list">
+          {items.map((item, index) => (
+            <li key={index} className="nav-horizontal__item">
+              {item.path ? (
+                <Link to={item.path} className="nav-horizontal__link">
+                  {item.text}
                 </Link>
-            }
+              ) : item.id ? (
+                <ScrollLinkToId
+                  to={item.id}
+                  className="nav-horizontal__link"
+                  {...scrollConfig}
+                >
+                  {item.text}
+                </ScrollLinkToId>
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      ) : null}
 
-            {items ?
-                <ul className="nav-horizontal__list">
-                    {items.map((item, index) => (
-                        <li key={index} className="nav-horizontal__item">
-                            {item.path ?
-                                <Link to={item.path} className="nav-horizontal__link">
-                                    {item.text}
-                                </Link> :
-                                item.id ?
-                                    <ScrollLinkToId to={item.id} className="nav-horizontal__link" {...scrollConfig}>
-                                        {item.text}
-                                    </ScrollLinkToId> :
-                                    null
-                            }
-                        </li>
-                    ))}
-                </ul> :
-                null
-            }
+      <div className="nav-horizontal__actions-box">
+        <button
+          className="plain-btn nav-horizontal__action-link"
+          onClick={cbChangeLanguage}
+        >
+          <span>{currentLanguage}</span>
 
+          {currentLanguage === "es" ? (
+            <ES className="nav-horizontal__flag-icon" />
+          ) : (
+            <US className="nav-horizontal__flag-icon" />
+          )}
+        </button>
 
-            <div className="nav-horizontal__actions-box">
-                <button className="plain-btn nav-horizontal__action-link" onClick={cbChangeLanguage}>
-                    <span>{currentLanguage}</span>
+        {!userIsLoggedIn && (
+          <Link
+            to="/login"
+            className="nav-horizontal__action-link nav-horizontal__action-link--login"
+          >
+            {loginText}
+          </Link>
+        )}
 
-                    {currentLanguage === "es" ?
-                        <ES className="nav-horizontal__flag-icon" /> :
-                        <US className="nav-horizontal__flag-icon" />
-                    }
-                </button>
+        {!userIsLoggedIn && (
+          <Link
+            to="/register"
+            className="nav-horizontal__action-link nav-horizontal__action-link--cta"
+          >
+            {signUpText}
+          </Link>
+        )}
 
-                {!userIsLoggedIn &&
-                    <Link to="/login" className="nav-horizontal__action-link">
-                        {loginText}
-                    </Link>
-                }
-
-                {!userIsLoggedIn &&
-                    <Link to="/register" className="nav-horizontal__action-link nav-horizontal__action-link--cta">
-                        {signUpText}
-                    </Link>
-                }
-
-                {userIsLoggedIn &&
-                    <Link to="/app/home" className="nav-horizontal__action-link nav-horizontal__action-link--cta nav-horizontal__action-link--app">
-                        app
-                    </Link>
-                }
-
-            </div>
-        </nav>
-    )
-}
+        {userIsLoggedIn && (
+          <Link
+            to="/app/home"
+            className="nav-horizontal__action-link nav-horizontal__action-link--cta nav-horizontal__action-link--app"
+          >
+            app
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default NavHorizontal;
