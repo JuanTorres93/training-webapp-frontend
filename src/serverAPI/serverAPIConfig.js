0c44225426f640ae50350c0198aa4752177d2c04
Juan Torres <juan93git@gmail.com>
2025-04-27 15:30:24 +0200

    Log for minikube

--- src/serverAPI/serverAPIConfig.js
+++ src/serverAPI/serverAPIConfig.js
@@ -1,25 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const serverBaseURL = process.env.REACT_APP_SERVER_BASE_URL;
+
+// TODO DELETE THESE DEBUG LOGS
+console.log("serverBaseURL");
+console.log(serverBaseURL);
 
 export const googleOAuthURL = `${serverBaseURL}/login/google`;
 
 // Create an instance of axios
 const apiClient = axios.create({
-    baseURL: serverBaseURL,
-    timeout: 10000, // Adjust timeout as needed
+  baseURL: serverBaseURL,
+  timeout: 10000, // Adjust timeout as needed
 });
 
 // Add a response interceptor
 apiClient.interceptors.response.use(
-    response => response, // Let other responses pass through
-    error => {
-        if (error.response && error.response.status === 429) {
-            // Handle 429 error globally
-            alert("You have sent too many requests. Please try again later.");
-        }
-        return Promise.reject(error); // Let other errors pass through
+  (response) => response, // Let other responses pass through
+  (error) => {
+    if (error.response && error.response.status === 429) {
+      // Handle 429 error globally
+      alert("You have sent too many requests. Please try again later.");
     }
+    return Promise.reject(error); // Let other errors pass through
+  }
 );
 
 export default apiClient;
