bf31f9eb726a4d93ea5da9f66b90c2bf6e6beb07
Juan Torres <juan93git@gmail.com>
2025-05-09 14:59:28 +0200

    Send welcome email on oauth registration

--- src/components/oauthLogin/OAuthLoginV2.js
+++ src/components/oauthLogin/OAuthLoginV2.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentLanguage } from "../../features/language/languageSlice";
 
-const OAuthLoginV2 = ({ logo, platformName, callbackURL, isEnabled = true }) => {
-    return (
-        <a
-            className={`plain-btn oauth-login ${isEnabled ? '' : 'oauth-login--disabled'}`}
-            href={callbackURL}
-            target="_blank"
-            aria-disabled={!isEnabled}
-            onClick={(e) => { if (!isEnabled) e.preventDefault(); }}
-        >
-            <figure className="oauth-login__icon-box">
-                <img src={logo} alt={`${platformName} logo`} className="oauth-login__icon" />
-            </figure>
+const OAuthLoginV2 = ({
+  logo,
+  platformName,
+  callbackURL,
+  isEnabled = true,
+}) => {
+  const currentLanguage = useSelector(selectCurrentLanguage);
 
-            <span className="oauth-login__text">
-                {platformName}
-            </span>
-        </a>
-    );
-}
+  return (
+    <a
+      className={`plain-btn oauth-login ${
+        isEnabled ? "" : "oauth-login--disabled"
+      }`}
+      href={callbackURL + `?state=lang:${currentLanguage}`}
+      target="_blank"
+      aria-disabled={!isEnabled}
+      onClick={(e) => {
+        if (!isEnabled) e.preventDefault();
+      }}
+    >
+      <figure className="oauth-login__icon-box">
+        <img
+          src={logo}
+          alt={`${platformName} logo`}
+          className="oauth-login__icon"
+        />
+      </figure>
+
+      <span className="oauth-login__text">{platformName}</span>
+    </a>
+  );
+};
 
 export default OAuthLoginV2;
